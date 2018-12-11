6b781a9f7cf7a68050b9f49b9c00f2bddac86829
Aloisio Sousa <aloisiocom@gmail.com>
2018-12-11 12:03:54 -0200

    Iniciando Boostrap

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Aula de Bootstrap</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 bg-danger" style=" height: 200px;"></div>
+			<div class="col-sm-4" style=" height: 200px; background-color: red;"></div>
+			<div class="col-sm-4" style=" height: 200px; background-color: yellow;"></div>
+		</div>
+	</div>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
